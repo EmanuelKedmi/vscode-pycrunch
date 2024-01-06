c2845b5c6927fda4846206a6d015ded6dc081815
Emanuel Kedmi <emanuel.kedmi@gmail.com>
2024-01-07 01:30:01 +0200

    less intrusive coverage bgcolor

--- src/decorations.ts
+++ src/decorations.ts
@@ -3,11 +3,11 @@ import * as vscode from 'vscode';
 const icons = ['covered', 'uncovered', 'partiallyCovered', 'errorPath', 'errorSource'] as const;
 export type IconDecorationDict = Record<typeof icons[number], vscode.TextEditorDecorationType>;
 const lineColors = {
-    covered: '#00ff0033',
-    uncovered: '#ffffff33',
-    partiallyCovered: '#ffff0033',
-    errorPath: '#aa000022',
-    errorSource: '#ff000033'  
+    covered: '#00ff0011',
+    uncovered: '#ffffff11',
+    partiallyCovered: '#ffff0011',
+    errorPath: '#aa000011',
+    errorSource: '#ff000011'  
 }
 
 export function createDecorations() {
